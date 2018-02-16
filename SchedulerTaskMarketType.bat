ed61a90de0ece013bf775926150d36870dc6488d
vzhomeexperiments <vz.home.experiments@gmail.com>
2018-02-16 19:36:14 +0100

    updated bat script

--- SchedulerTaskMarketType.bat
+++ SchedulerTaskMarketType.bat
@@ -1,2 +1,5 @@
 @echo off
-"C:\Program Files\R\R-3.4.3\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\000_TradingRepo\R_markettype\8_ScoreData.R"
+::using R.exe will produce R.out file where we can read diagnostics of the code
+::"C:\Program Files\R\R-3.4.3\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\000_TradingRepo\R_markettype\8_ScoreData.R"
+::alternative way is to use Rscript.exe program
+"C:\Program Files\R\R-3.4.3\bin\Rscript.exe" "C:\Users\fxtrams\Documents\000_TradingRepo\R_markettype\8_ScoreData.R"
