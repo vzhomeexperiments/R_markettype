95a72a19010861d16955c9dc93d20dc50c7ffcf8
vzhomeexperiments <vz.home.experiments@gmail.com>
2018-04-03 20:26:21 +0200

    deleting unused files

--- SchedulerTaskMarketType.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-::using R.exe will produce R.out file where we can read diagnostics of the code
-::"C:\Program Files\R\R-3.4.3\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\000_TradingRepo\R_markettype\8_ScoreData.R"
-::alternative way is to use Rscript.exe program
-"C:\Program Files\R\R-3.4.3\bin\Rscript.exe" "C:\Users\fxtrams\Documents\000_TradingRepo\R_markettype\8_ScoreData.R"
