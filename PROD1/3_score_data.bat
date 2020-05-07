4adc057129b896a587b42795b94d1be1046e8a77
vzhomeexperiments <vz.home.experiments@gmail.com>
2020-05-07 11:03:44 +0200

    go for R 4.0.0

--- PROD1/3_score_data.bat
+++ PROD1/3_score_data.bat
@@ -2,4 +2,4 @@
 :: Use this line of code for debugging purposes *** Adapt the paths for R installation and R script!!!
 ::"C:\Program Files\R\R-3.6.2\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\000_TradingRepo\R_markettype\PROD1\3_score_data.R"
 :: Use this code in 'production'
-"C:\Program Files\R\R-3.6.2\bin\Rscript.exe" "C:\Users\fxtrams\Documents\000_TradingRepo\R_markettype\PROD1\3_score_data.R"
+"C:\Program Files\R\R-4.0.0\bin\Rscript.exe" "C:\Users\fxtrams\Documents\000_TradingRepo\R_markettype\PROD1\3_score_data.R"
